435327736762e2e0a69623708948b7111a12bd8c
tyrionyu <tyrionyu@foxmail.com>
2024-09-30 13:59:57 +0800

    python

--- chapter05/示例5-06列表的编历操作.py
+++ chapter05/示例5-06列表的编历操作.py
@@ -1,6 +1,6 @@
 lst=['hello','world','python','php']
-for item in lst:
-    print(item)
+# for item in lst:
+#     print(item)
 
 # 使用for循环，range()函数，len()函数，根据索引进行编历
 for i in range(0,len(lst)):
@@ -10,9 +10,9 @@ for i in range(0,len(lst)):
 for index,item in enumerate(lst):
     print(index,item) #index是序列，不是索引
 
-#手动修改序号的起始值
-for index,item in enumerate(lst,start=1):
-    print(index,item)
+# # #手动修改序号的起始值
+# # for index,item in enumerate(lst,start=1):
+# #     print(index,item)
 
-for index,item in enumerate(lst,1):
-    print(index,item) 
+# for index,item in enumerate(lst,1):
+#     print(index,item) 
