324363537823b4a5bb700f5c023839d87aa0e2e9
tyrionyu <tyrionyu@foxmail.com>
2025-07-17 14:05:32 +0800

    python

--- Test/4.0.15斐波那契数列.py
+++ Test/4.0.15斐波那契数列.py
@@ -1,6 +1,6 @@
 #   1	1	2	3	5	8	13	21	34	55	89	​​144​
 
-""" # 斐波那契数列
+# 斐波那契数列
 def fibonacci_iterative(n):
     a, b = 0, 1
     for _ in range(n):
@@ -8,4 +8,4 @@ def fibonacci_iterative(n):
     return a
 
 # 计算第12项
-print(fibonacci_iterative(12))  # 输出: 144 """
+print(fibonacci_iterative(12))  # 输出: 144
