7bd823f3a2cab614a909b891f83899d31d9e69f3
tyrionyu <tyrionyu@foxmail.com>
2025-07-23 14:07:17 +0800

    python

--- book/3.0.5修改嘉宾名单.py
+++ book/3.0.5修改嘉宾名单.py
@@ -10,10 +10,11 @@ name=guest[2]
 print(f"{name},邀请你和我一起晚餐")
 
 name=guest[1]
-print(f"{name},没有办法来参加晚餐")
+print(f"抱歉{name},我没有办法来参加晚餐")
 
 del(guest[1])
 guest.insert(1,"老六")
+
 name=guest[1]
 print(f"{name},邀请你和我一起晚餐")
 print(guest)
