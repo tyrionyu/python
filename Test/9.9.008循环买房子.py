22e9eaf15be3bc8f4fe8f536bd730a661ccbd09e
tyrionyu <tyrionyu@foxmail.com>
2025-08-20 20:48:59 +0800

    python

---
+++ Test/9.9.008循环买房子.py
@@ -0,0 +1,31 @@
+'''
+描述
+某程序员开始工作，年薪N万，他希望在中关村公馆买一套60平米的房子，现在价格是200万，假设房子价格以每年百分之K增长，并且该程序员未来年薪不变，且不吃不喝，不用交税，每年所得N万全都积攒起来，问第几年能够买下这套房子？（第一年年薪N万，房价200万）
+
+输入
+一行，包含两个正整数N（10 <= N <= 50）, K（1 <= K <= 20），中间用单个空格隔开。
+输出
+如果在第20年或者之前就能买下这套房子，则输出一个整数M，表示最早需要在第M年能买下，否则输出Impossible。
+样例输入
+50 10
+样例输出
+8
+'''
+N, K = map(int, input().split())
+
+house_price = 200.0
+savings = 0.0
+found = False
+year = 0
+
+for year in range(1, 21):
+    savings += N
+    if savings >= house_price:
+        found = True
+        break
+    house_price *= (1 + K / 100)
+
+if found:
+    print(year)
+else:
+    print("Impossible")
